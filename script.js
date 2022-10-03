406b0a66bb5e3ea7d322fef45ea05cf9aa0899a7
ShirshoDipto <shirshodipto@gmail.com>
2022-10-03 13:34:30 +0600

    Fix repeated clicking DOM issue

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
+numClicks = 0;
+numItems = 0;
 
-let userInput = 0
+
+function getUserInput() {
+
+    let userInput = parseInt(prompt("Enter the sketchpad size. (e.g. for a 16*16 size, enter 16. DO NOT ENTER MORE THAN 100.", "16"));
+    console.log(userInput);
+
+    while (userInput > 100) {
+
+        userInput = parseInt(prompt("DO NOT ENTER MORE THAN 100. Try again, you can do it", ));
+        console.log(userInput);
+
+        if (userInput < 100) {
+            break;
+        };
+    }
+    return userInput;
+}
 
 
-function makeGrid(numGrid) {
+function removeExistingDom() {
 
-    if ()
+}
+
+
+function makeGrid(e) {
+    numClicks++;
+
+    // remove existing DOM.
+    const numGrid = getUserInput();
+    if (numClicks > 1) {
+        const sketchpad = document.querySelector('.sketch-pad');
+        for (let i = 0; i < numItems; i++) {
+            const parentDiv = document.querySelector('.parentDiv');
+            sketchpad.removeChild(parentDiv);
+        }
+    }
+    
     const widthHeight = 850 / numGrid;
     const widthHeightPx = widthHeight.toString() + 'px';
     const sketchpad = document.querySelector('.sketch-pad');
@@ -23,7 +56,7 @@ function makeGrid(numGrid) {
             parentDiv.appendChild(div);
         }
     }
-
+    numItems = numGrid;
 }
 
 // tests for makeGrid function.
@@ -31,25 +64,6 @@ function makeGrid(numGrid) {
 
 
 
-function getUserInput() {
-
-    userInput = parseInt(prompt("Enter the sketchpad size. (e.g. for a 16*16 size, enter 16. DO NOT ENTER MORE THAN 100.", "16"));
-    console.log(input);
-
-    while (input > 100) {
-
-        userInput = parseInt(prompt("DO NOT ENTER MORE THAN 100. Try again, you can do it", ));
-        console.log(input);
-
-        if (input < 100) {
-            break;
-        };
-    }
-    makeGrid(userInput);
-
-}
-
-
 const input = document.querySelector('.input');
 
-input.addEventListener('click', getUserInput);
+input.addEventListener('click', makeGrid);
